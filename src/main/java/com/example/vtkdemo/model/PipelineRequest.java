11b2dec8c57b6de86ca8ff7a7d47df39bfb2d8a9
Gustavo Tajes Genga <gtajsgena@alumnos.exa.unicen.edu.ar>
2019-11-13 18:59:20 -0300

    13112019

--- src/main/java/com/example/vtkdemo/model/PipelineRequest.java
+++ src/main/java/com/example/vtkdemo/model/PipelineRequest.java
@@ -1,11 +1,13 @@
 package com.example.vtkdemo.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor
 public class PipelineRequest {
 
     @NonNull
